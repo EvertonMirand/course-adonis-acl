6be6be7b41547fada47ed8301488782e55d31628
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-20 22:17:09 -0300

    Ajustando o UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,27 +1,59 @@
-'use strict'
+"use strict";
 
-const User = use('App/Models/User')
+const User = use("App/Models/User");
 
 class UserController {
-  async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+  async store({ request }) {
+    const { permissions, roles, ...data } = request.only([
+      "username",
+      "email",
+      "password",
+      "permissions",
+      "roles"
+    ]);
 
-    const user = await User.create(data)
+    const user = await User.create(data);
 
-    return user
+    if (roles) {
+      await user.roles().attach(roles);
+    }
+
+    if (permissions) {
+      await user.permissions().attach(permissions);
+    }
+
+    await user.loadMany(["roles", "permissions"]);
+
+    return user;
   }
 
-  async update ({ request, params }) {
-    const data = request.only(['username', 'email', 'password'])
+  async update({ request, params }) {
+    const { permissions, roles, ...data } = request.only([
+      "username",
+      "email",
+      "password",
+      "permissions",
+      "roles"
+    ]);
 
-    const user = await User.findOrFail(params.id)
+    const user = await User.findOrFail(params.id);
 
-    user.merge(data)
+    user.merge(data);
 
-    await user.save()
+    await user.save();
 
-    return user
+    if (roles) {
+      await user.roles().sync(roles);
+    }
+
+    if (permissions) {
+      await user.permissions().sync(permissions);
+    }
+
+    await user.loadMany(["roles", "permissions"]);
+
+    return user;
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
